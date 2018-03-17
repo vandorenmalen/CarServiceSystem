c9c677031c7604edb2e53a9c01174dea3de29d88
Marc van Dorenmalen <mvandorenmalen@outlook.com>
2018-03-17 16:31:56 +0100

    Add project files.

---
+++ CarServiceSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarServiceSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
